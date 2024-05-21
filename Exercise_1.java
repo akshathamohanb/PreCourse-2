74710590cb6814ecd84bb76666be29b3f39c5063
Akshatha Mohan <akshatha.mohanb@gmail.com>
2024-05-21 17:19:06 -0500

    Done PreCourse2

--- Exercise_1.java
+++ Exercise_1.java
@@ -2,7 +2,18 @@ class BinarySearch {
     // Returns index of x if it is present in arr[l.. r], else return -1 
     int binarySearch(int arr[], int l, int r, int x) 
     { 
-        //Write your code here
+        if (r>=1) {
+            int mid = l + (r-1) / 2;
+            if (arr[mid] == x)
+                return mid;
+
+            if (arr[mid]>x)
+                return binarySearch(arr,1,mid-1,x);
+
+            return binarySearch(arr,mid+1,r,x);
+
+        }
+        return -1;
     } 
   
     // Driver method to test above 
--- Exercise_2.java
+++ Exercise_2.java
@@ -8,11 +8,30 @@ class QuickSort
        of pivot */
     void swap(int arr[],int i,int j){
         //Your code here   
+        if (i!= j) {
+            arr[i] = arr[i] ^ arr[j];
+            arr[j] = arr[i] ^ arr[j];
+            arr[i] = arr[i] ^ arr[j];
+        }
     }
     
     int partition(int arr[], int low, int high) 
     { 
-   	//Write code here for Partition and Swap 
+   	//Write code here for Partition and Swap
+        int pivot = arr[high];
+        int i = (low-1);
+        for (int j=low;j<high;j++)
+        {
+            if (arr[j] <= pivot)
+            {
+                i++;
+                swap(arr,i,j);
+            }
+        }
+        swap(arr,i +1, high);
+
+        return i+1;
+    
     } 
     /* The main function that implements QuickSort() 
       arr[] --> Array to be sorted, 
@@ -22,6 +41,13 @@ class QuickSort
     {  
             // Recursively sort elements before 
             // partition and after partition 
+            if (low<high)
+            { 
+                int pi = partition(arr,high,low);
+                sort(arr,low,pi-1);
+                sort(arr,pi+1, high);
+
+            }
     } 
   
     /* A utility function to print array of size n */
